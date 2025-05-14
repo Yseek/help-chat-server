4ca1b253f38b6e04583bb9da1c0336ea70f1e317
yun <yun5003@naver.com>
2025-05-14 11:09:20 +0000

    ✨ Login 통신 테스트 Controller 추가

---
+++ src/main/java/chat/help/help_chat/controller/LoginController.java
@@ -0,0 +1,30 @@
+package chat.help.help_chat.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:3000")
+public class LoginController {
+    
+    @PostMapping
+    public Mono<ResponseEntity<LoginResponse>> postMethodName(@RequestBody LoginRequest entity) {
+
+        //TODO: process POST request
+        System.out.println(entity.email() + " " + entity.password());
+
+        return Mono.just(ResponseEntity.ok(new LoginResponse(entity.email(), "tokenTest")));
+    }
+
+    record LoginRequest(String email, String password) {}
+    record LoginResponse(String email, String token) {}
+}

39cebd25a933d10a919047dc2415111142393050
yseek <yun5003@naver.com>
2025-05-14 05:33:06 +0000

    🎉 Help-chat-server 프로젝트 시작

---
+++ src/main/java/chat/help/help_chat/HelpChatApplication.java
@@ -0,0 +1,13 @@
+package chat.help.help_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpChatApplication.class, args);
+	}
+
+}
